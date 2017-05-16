3a9f6ad557d7bd0a8475dc2949278c563081e000
squoraishee <coruscatingluminostiy@gmail.com>
2017-05-16 14:41:07 -0400

    Adding Last

---
+++ app/src/main/java/jwtest/webview/com/myapplication/MainActivity.java
@@ -0,0 +1,96 @@
+package jwtest.webview.com.myapplication;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    private JWPlayerView playerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String jwplayer = "<html><body><div style='position:relative; overflow:hidden;'><iframe src='https://content.jwplatform.com/players/Rd8LnswY-RO5tFT3n.html' width='200' height='200' frameborder='0' scrolling='auto'></iframe></div></body></html>";
+        String youtube = "<iframe id='ytplayer' type='text/html' width='640' height='360' src='https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com' frameborder='0'></iframe>";
+
+        webView = (WebView)findViewById(R.id.test_webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        webView.loadData(jwplayer,  "text/html; charset=UTF-8", null);
+//        loadNativePlayer();
+
+
+//        webView.setOnScrollChangedCallback(new ObservableWebView.OnScrollChangedCallback() {
+//                @Override
+//                public void onScroll(int l, int t) {
+//                    toast("Scroll callback");
+//                }
+//            });
+//
+//            String script
+//                    = "function f(){ var r = document.getElementById('%@').getBoundingClientRect(); " +
+//                    "return '{{'+r.left+','+r.top+'},{'+r.width+','+r.height+'}}'; } f();";
+//
+//            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//                webView.evaluateJavascript(script, new ValueCallback<String>() {
+//                    @Override
+//                    public void onReceiveValue(String value) {
+//                        toast("Result: " + value);
+//                    }
+//                });
+//            }
+
+        //webView.loadData(youtube,  "text/html; charset=UTF-8", null);
+    }
+
+    public void loadNativePlayer() {
+        playerView.setVisibility(View.VISIBLE);
+        playerView = (JWPlayerView) findViewById(R.id.playerView);
+        PlaylistItem pi = new PlaylistItem.Builder()
+                        .file("https://content.jwplatform.com/manifests/gFGOwEll.m3u8")
+                        .title("BipBop")
+                        .description("A video player testing video.")
+                        .build();
+        playerView.load(pi);
+        playerView.play();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //playerView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        //playerView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        //playerView.onDestroy();
+        super.onDestroy();
+    }
+
+    public void toast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/jwtest/webview/com/myapplication/ObservableWebView.java
@@ -0,0 +1,50 @@
+package jwtest.webview.com.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+/**
+ * Created by squoraishee on 5/9/17.
+ */
+
+public class ObservableWebView extends WebView
+{
+    private OnScrollChangedCallback mOnScrollChangedCallback;
+
+    public ObservableWebView(final Context context)
+    {
+        super(context);
+    }
+
+    public ObservableWebView(final Context context, final AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public ObservableWebView(final Context context, final AttributeSet attrs, final int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onScrollChanged(final int l, final int t, final int oldl, final int oldt)
+    {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if(mOnScrollChangedCallback != null) mOnScrollChangedCallback.onScroll(l, t);
+    }
+
+
+    public void setOnScrollChangedCallback(final OnScrollChangedCallback onScrollChangedCallback)
+    {
+        mOnScrollChangedCallback = onScrollChangedCallback;
+    }
+
+    /**
+     * Impliment in the activity/fragment/view that you want to listen to the webview
+     */
+    public interface OnScrollChangedCallback
+    {
+        public void onScroll(int l, int t);
+    }
+}
